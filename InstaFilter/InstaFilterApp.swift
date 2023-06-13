f62939b9539c111ad8a0949376d3b62192a4e1aa
Tochukwu Ukpai <tochukwuukpai@gmail.com>
2023-06-13 14:43:39 +0100

    Initial Commit

---
+++ InstaFilter/InstaFilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaFilterApp.swift
+//  InstaFilter
+//
+//  Created by Theós on 13/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct InstaFilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
